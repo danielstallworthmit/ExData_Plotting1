71a99212fbeed95a23cded098334eeeb1bb6f789
Daniel Stallworth <danielstallworthmit@gmail.com>
2017-04-09 12:45:30 -0500

    Saved filtered data to csv for future plots, plot2 GAP by datetime saved to png

--- plot1.R
+++ plot1.R
@@ -4,6 +4,7 @@ data <- fread("/Users/danielstallworth/Downloads/household_power_consumption.txt
 data$Date <- as.Date(data$Date,format = "%d/%m/%Y")
 setkey(data,Date)
 data <- data[Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02")]
+write.csv(data,file = "household_consumption_filtered.csv",row.names = F)
 
 # Plotting the histogram and put graph in 480x480 png
 png("plot1.png", width = 480, height = 480)
---
+++ plot2.R
@@ -0,0 +1,12 @@
+library(data.table)
+# Set up data using filtered data from plot1
+data_filtered <- fread("household_consumption_filtered.csv",sep = ",",header = T)
+data_filtered <- within(data_filtered, DateTime <- paste(Date, Time, sep = " "))
+data_filtered$DateTime <- as.POSIXct(data_filtered$DateTime,tz = "GMT")
+
+# Plotting week day and GAP in 480x480 png
+png("plot2.png", width = 480, height = 480)
+with(data_filtered, plot(DateTime, Global_active_power, type = "l",
+                xlab="",
+                ylab="Global Active Power (kilowatts)"))
+dev.off()
