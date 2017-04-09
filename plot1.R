b043a50ff85cbdb23c929c48122ca8c0b9cf924b
Daniel Stallworth <danielstallworthmit@gmail.com>
2017-04-09 12:46:35 -0500

    Update plot1.R file path

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 library(data.table)
 # Set up data
-data <- fread("/Users/danielstallworth/Downloads/household_power_consumption.txt",sep = ";",na.strings = "?",header = T)
+data <- fread("household_power_consumption.txt",sep = ";",na.strings = "?",header = T)
 data$Date <- as.Date(data$Date,format = "%d/%m/%Y")
 setkey(data,Date)
 data <- data[Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02")]
